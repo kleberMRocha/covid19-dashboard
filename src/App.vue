888cda4474668fb3d8f968fe6c10bc788629b0b3
Kleber Rocha <kleberrocha2701@gmail.com>
2022-11-18 12:20:45 -0300

    add axios e vuetifyjs

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,22 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
+  <v-app id="inspire">
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col
+            v-for="n in 24"
+            :key="n"
+            cols="4"
+          >
+            <v-card height="200"></v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+   
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
