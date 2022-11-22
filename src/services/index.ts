08e010d4771104badd8ea656791c0f47307aa42d
Kleber Rocha <kleberrocha2701@gmail.com>
2022-11-22 16:30:20 -0300

    add services routes

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosResponse } from "axios";
-import { IResponseData } from "../@types/type";
+import { IInfoUF, IResponseData } from "../@types/type";
 
 const Api = axios.create({
     baseURL: 'https://covid19-brazil-api.now.sh/api/report/v1',
@@ -9,6 +9,14 @@ const Api = axios.create({
 export const Requests =  {
     getCurrent: async () => {
         const result: AxiosResponse<IResponseData> = await Api.get('/');
+        return result.data;
+    },
+    getCurrentByUf: async (uf:string) => {
+        const result: AxiosResponse<IInfoUF> = await Api.get(`brazil/uf/${uf}`);
+        return result.data;
+    },
+    getByDate: async (date:string) => {
+        const result: AxiosResponse<IResponseData> = await Api.get(`brazil/${date}`);
         return result.data;
     }
 };
